e238d397d4c276dbfcf9dd52a4c760d0dec4ba46
jannick holm <jannickholm@computeriana.kea.local>
2019-10-14 11:42:11 +0200

    react-half-way

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+function Header(props) {
+	return (
+		<header>
+			<p>Header</p>
+			<Profile name={props.name} />
+		</header>
+	);
+}
+
+function Profile(props) {
+	return (
+		<div className="profile">
+			<p>Hello, {props.name}</p>
+		</div>
+	);
+}
+
+function Feed(props) {
+	return (
+		<div className="feed">
+			<p>Feed</p>
+			<AddPostForm />
+			{props.posts}
+		</div>
+	);
+}
+
+function AddPostForm() {
+	return (
+		<form>
+			<p>Add post form</p>
+		</form>
+	);
+}
+
+function Post() {
+	const [ count, setCount ] = useState(0);
+	const updateCount = () => {
+		setCount(count + 1);
+	};
+
+	return (
+		<div className="post">
+			<p>Post</p>
+			<p>This post have {count} likes</p>
+			<button onClick={updateCount}>Like</button>
+			<AddCommentForm />
+			<Comment />
+			<Comment />
+		</div>
+	);
+}
+
+function AddCommentForm() {
+	return (
+		<form>
+			<p>Add Comment Form</p>
+		</form>
+	);
+}
+
+function Comment() {
+	return (
+		<div className="comment">
+			<p>Comment</p>
+		</div>
+	);
+}
+
+function Footer(props) {
+	return (
+		<footer>
+			<p>{props.name}</p>
+		</footer>
+	);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const name = 'Jannick'; //Pass to profile and footer
+	const post = [ <Post />, <Post />, <Post /> ]; //Pass to feed
+
+	return (
+		<div className="App">
+			<Header name={name} />
+			<Feed posts={post} />
+			<Footer name={name} />
+		</div>
+	);
 }
 
 export default App;
